cadd45253f9f9139d1cd4cd15c200ac4873d080f
Баханец Анна <a.bakhanec@2gis.ru>
2023-02-16 11:55:59 +0400

    install requirement in setup

--- setup.py
+++ setup.py
@@ -2,4 +2,12 @@ from __future__ import annotations
 
 from setuptools import setup
 
-setup()
+
+def find_required():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
+
+setup(
+    version='0.0.1',
+    install_requires=find_required(),
+)
